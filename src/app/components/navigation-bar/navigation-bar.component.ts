f8fdeb538ade5099c17ec07863f4f3a78a933768
Darashc <darashc6@gmail.com>
2021-11-25 20:26:38 +0100

    Favicon
    - Replaced favicon
    - Minor styling changes to the navigation bar
    - Push changes to GitHub Pages

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -6,22 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-bar.component.css']
 })
 export class NavigationBarComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
   goToSection(section_name: string) {
-    document.querySelector('.navbar-items')?.classList.toggle('open');
-    document.querySelector('.navbar-background')?.classList.toggle('navbar-open');
+    document.querySelector('.navbar-items')?.classList.toggle('navbar-active');
+    document.querySelector('.navbar-background')?.classList.toggle('navbar-active');
     document.getElementById(section_name)?.scrollIntoView({
       behavior: "smooth"
     });
   }
 
   toggleNavigationBar() {
-    document.querySelector('.navbar-items')?.classList.toggle('open');
-    document.querySelector('.navbar-background')?.classList.toggle('navbar-open');
+    document.querySelector('.navbar-items')?.classList.toggle('navbar-active');
+    document.querySelector('.navbar-background')?.classList.toggle('navbar-active');
   }
 }
